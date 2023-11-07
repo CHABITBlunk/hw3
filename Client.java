54eb3c00e48f79b3750e3df036a982c37f8e8d2d
xfbm <cblunk097@gmail.com>
2023-11-06 18:50:13 -0800

    Support two clients before terminating; write out port number & random number before moving to next client

--- Client.java
+++ Client.java
@@ -1,39 +1,34 @@
 package cs250.hw3;
 
-import java.io.*;
 import java.net.*;
+import java.io.*;
 
 public class Client {
-	
 	private static Socket socket;
 	private static DataInputStream din = null;
 
 	public static void main(String[] args) {
-		// java Client <address> <port>
 		String address = null;
 		int portNumber = Integer.MAX_VALUE;
-		
 		if (args.length == 2) {
 			address = args[0];
 			portNumber = Integer.parseInt(args[1]);
 		} else {
-			System.err.println("Incorrect number of arguments. 2 arguments required. java Client <address> <port-number>");
+			System.err.println("Incorrect Number of Arguments. 2 Arguments Required. java Client <address> <portNumber>");
 			System.exit(1);
 		}
-		
 		try {
-		  	socket = new Socket(address, portNumber);
-			System.out.println("Connected to server!");			
-			
+			socket = new Socket(address, portNumber);
+			System.out.println("Connected to Server!");
 			din = new DataInputStream(socket.getInputStream());
-			int message = din.readInt();
-			System.out.println("Message from server: " + message);
+			int firstInt = din.readInt();
+			int secondInt = din.readInt();
+			System.out.println("Message from server: " + firstInt);
+			System.out.println("Message from server: " + secondInt);
 			System.out.println("Terminating.");
 		} catch (IOException e) {
-			System.err.println("Fatal connection error");
+			System.err.println("Fatal Connection Error!");
 			e.printStackTrace();
 		}
-
 	}
-
 }
--- Server.java
+++ Server.java
@@ -1,42 +1,47 @@
 package cs250.hw3;
 
-import java.io.*;
 import java.net.*;
+import java.io.*;
+import java.util.Random;
 
 public class Server {
-	
-	private static Socket clientSocket;
-	private static ServerSocket serverSocket; 
-	private static DataOutputStream doutClient;
+	private static Socket clientSocketOne;
+	private static Socket clientSocketTwo;
+	private static ServerSocket serverSocket;
+	private static DataOutputStream doutClient = null;
 
 	public static void main(String[] args) {
-		// java Server <port-number>
-		int portNumber = Integer.MAX_VALUE;
+		int portNumber = Integer.MAX_VALUE; // Garbage value so we don't start server on random port.
 		if (args.length == 1) {
 			portNumber = Integer.parseInt(args[0]);
 		} else {
-			System.err.println("Incorrect number of arguments. 1 argument required. java Server <port-number>");
+			System.err.println("Incorrect Number of Arguments. 1 Argument Required. java Server <portNumber>");
 			System.exit(1);
 		}
-		
-		// listen on given port
 		try {
 			serverSocket = new ServerSocket(portNumber);
-			System.out.println("Waiting on client");
-			// wait for client
-			clientSocket = serverSocket.accept();
-			System.out.println("Connected to client");
-			
-			int theAnswer = 42;
-			doutClient = new DataOutputStream(clientSocket.getOutputStream());
-			doutClient.writeInt(theAnswer);
+			Random random = new Random();
+			// wait on client
+			clientSocketOne = serverSocket.accept();
+			System.out.println("Connected to Client 1!");
+			doutClient = new DataOutputStream(clientSocketOne.getOutputStream());
+			doutClient.writeInt(portNumber);
 			doutClient.flush();
-			
-			
+			doutClient.writeInt(random.nextInt());
+			doutClient.flush();
+			System.out.println("Information Sent to Client 1.");
+
+			clientSocketTwo = serverSocket.accept();
+			System.out.println("Connected to Client 2!");
+			doutClient = new DataOutputStream(clientSocketTwo.getOutputStream());
+			doutClient.writeInt(portNumber);
+			doutClient.flush();
+			doutClient.writeInt(random.nextInt());
+			doutClient.flush();
+			System.out.println("Information Sent to Client 2.");
 		} catch (IOException e) {
-			System.err.printf("Could not listen on port %d\n", portNumber);
+			System.err.println("Could not listen on port: " + portNumber);
 			System.exit(1);
 		}
 	}
-
 }
